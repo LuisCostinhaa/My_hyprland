4872bc6fa02f30b98e90ef1c9ca0125ac5afaa14
LuisCostinhaa <costinhaa@gmail.com>
2025-12-13 18:15:51 +0000

    2025-12-13

--- install.sh
+++ install.sh
@@ -76,7 +76,7 @@ sudo systemctl enable ufw.service
 ######### Setings #####
 xdg-user-dirs-update
 cd sddm
-sudo cp -r theme.conf /usr/share/sddm/themes/Sugar-Candy
+sudo cp -r ./* /usr/share/sddm/themes/Sugar-Candy
 cd ..
 sudo cp -r backgrounds ~/Imagens/
 sudo cp -r sddm.conf /etc/
